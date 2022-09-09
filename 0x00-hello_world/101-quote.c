22fa225be508edb6aa67f9fb25016743e5da95c4
Abounazir <ola.oyetoro@gmail.com>
2022-09-09 03:40:48 -0700

    Betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <unistd.h>
 
 /**
- *Main-program entry point.
+ * Main-This is the program entry point.
  *
- *Return-Return 1 on success.
+ * Description-This program prints exactly
+ * "and that piece of art is useful" -
+ * Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
+ * Return-This program should return 1 on success.
  */
 
 int main(void)
