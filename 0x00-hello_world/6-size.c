2db9bfde60b9166680a381413fd349d1d694c893
Abounazir <ola.oyetoro@gmail.com>
2022-09-09 03:20:18 -0700

    Betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
-
 /**
-
- *main -is the entry point for the program.
-
+ * main - serves as the entry point for the program
  *
-
- *Return:zero if no error, Non-zero value  if errors.
-
+ * Description -The return value for main is used to indicate
+ * how the program exited. If the program execution
+ * was normal, a 0 return value is used.
+ * Abnormal termination(errors, invalid inputs, segmentation
+ * faults, etc.) is usually terminated by a non-zero return.
+ *  Return: returns zero by default if there are no errors
+ * otherwise a non zero value is returned
  */
-
 int main(void)
-
 {
-
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
-
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
-
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 
 	return (0);
-
 }
