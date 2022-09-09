56e069fa06dc5171d9cb6a4e979f6bbddd3d0cf2
Abounazir <ola.oyetoro@gmail.com>
2022-09-08 19:26:34 -0700

    Another commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,9 @@
 
 int main(void)
 	/*main - serves as the entry point of the program*/
-
 {
-
-	puts("Programming is like building a multilingual puzzle\n");
-
+	puts("/"Programming is like building a multilingual puzzle/"\n");
 	return (0);
-	
-		
 }
 	/*return - returns zero by default if there are no errors otherwise return zero value*/
 
