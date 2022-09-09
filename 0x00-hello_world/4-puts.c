0fce8758aab15b34e07f0669df96fadcc2620768
Abounazir <ola.oyetoro@gmail.com>
2022-09-08 21:13:36 -0700

    Finishing touches to 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * main - serves as the entry point of the program
- * return - returns zero by default if there are no errors otherwise return a nonon-zero value
+ * return - returns zero by default if there are no errors 
+ * otherwise return a non-zero value
 */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts ("\"Programming is like building a multilingual puzzle\"");
 	return(0);
 }
