59cdf6b546b25f51a3bca3cd102773f888a1ee62
Abounazir <ola.oyetoro@gmail.com>
2022-09-08 21:50:45 -0700

    Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 /**
  * Main - It serves as the entry point of the program
  *
- * Main serves as the entry point of the program
+ * Main serves as the entry point of the program and it is
+ * meant to print a result when it is done.
  *
  * Return: returns zero by default if there are no errors
  * otherwise return a non-zero value
