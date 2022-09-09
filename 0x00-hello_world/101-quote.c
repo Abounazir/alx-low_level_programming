4c49d369612d85adfdf6d28cb554895bc18ae5f8
Abounazir <ola.oyetoro@gmail.com>
2022-09-09 04:36:33 -0700

    Betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  *main-This is the program entry point.
  *
- *Description-Function Main  prints exactly
- *"and that piece of art is useful" -
- *Dora Korpar, 2015-10-19,
- *followed by a new line, to the standard error.
+ *Description-Function Main prints exactly
+ *and that piece of art is useful -
+ *Dora Korpar, followed by a new line,
+ *to the standard error.
  *return value of main-This time, we return an error value of 1.
  */
 
