17826eaee1ecf35c41aab366fa2f72d2f1622f63
Abounazir <ola.oyetoro@gmail.com>
2022-09-09 02:18:51 -0700

    Finally 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 /**
- * Main - It serves as the entry point of the program
+ * main - serves as the entry point for the program
  *
- * Main serves as the entry point of the program and it is
- * meant to print a result when it is done.
- *
- * Return: returns zero by default if there are no errors
- * otherwise return a non-zero value
-*/
+ * return - By default, return zero if there are no errors
+ * Otherwise return a non-zero vaue
+ */
 int main(void)
-/* Main serves as the entry point of the program*/
+	
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
